ee52175acf672d413316e6c77509f5258fcafb0e
Karim KOLLI <kakolli@airfrance.fr>
2020-02-05 14:03:31 +0100

    add conditional validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, EmailValidator} from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password.validator';
 
@@ -9,27 +9,52 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup;
+
+  ngOnInit(){
+    this.registrationForm = this._formBuilder.group({
+      userName: ['Jane Doe', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: ['toto'],
+      confirmPassword: ['toto'],
+      address: this._formBuilder.group({
+        street: ['Maxwell avenue'],
+        city: ['Toulouse'],
+        postalCode: ['31109']
+      })
+    }, {validator: PasswordValidator});
+  
+
+    this.registrationForm.get('subscribe').valueChanges.subscribe( checkedValue => {
+      const email = this.registrationForm.get('email');
+      if (checkedValue) {
+        
+        email.setValidators(Validators.required);
+      } else {
+        email.clearValidators();
+      }
+
+      email.updateValueAndValidity();
+    })
+  }
+
   constructor(private _formBuilder: FormBuilder){}
 
   title = 'reactive-forms';
 
-  registrationForm = this._formBuilder.group({
-    userName: ['Jane Doe', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
-    password: ['toto'],
-    confirmPassword: ['toto'],
-    address: this._formBuilder.group({
-      street: ['Maxwell avenue'],
-      city: ['Toulouse'],
-      postalCode: ['31109']
-    })
-  }, {validator: PasswordValidator});
-
+  
 
   get userName() {
     return this.registrationForm.get('userName');
   }
 
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
 
   // registrationForm = new FormGroup({
   //   userName: new FormControl('John Smith'),
